fca7bdf66490f340ab58d265de3aa52b77de3761
ikaro-lisarb <ikaro.lisarb@proton.me>
2025-08-19 17:14:30 -0300

    feat: add product creation endpoint with Mapster and Carter integration

--- src/Services/Catalog/CatalogAPI/Program.cs
+++ src/Services/Catalog/CatalogAPI/Program.cs
@@ -1,11 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the DI container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
-
-// Configure HTTP enpoints
-
+// Configure HTTP endpoints
+app.MapCarter();
 
 app.Run();
