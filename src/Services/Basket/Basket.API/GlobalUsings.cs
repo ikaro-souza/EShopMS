cfa436fdc33ecbc7a1001d4a687791736a1e47be
ikaro-lisarb <ikaro.lisarb@proton.me>
2025-09-13 19:45:48 -0300

    feat: add health checks and configure MediatR with exception handling

--- src/Services/Basket/Basket.API/GlobalUsings.cs
+++ src/Services/Basket/Basket.API/GlobalUsings.cs
@@ -1,6 +1,7 @@
 global using Carter;
 global using MediatR;
 global using Mapster;
+global using Marten;
 global using FluentValidation;
 global using BuildingBlocks.CQRS;
 global using BuildingBlocks.Behaviors;
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,7 +1,34 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var assembly = typeof(Program).Assembly;
+
 builder.Services.AddCarter();
+builder.Services.AddValidatorsFromAssembly(assembly);
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+// var connectionString = builder.Configuration.GetConnectionString("Database")!;
+// builder.Services.AddMarten(config => { config.Connection(connectionString); })
+//     .UseLightweightSessions();
+//
+// builder.Services.AddHealthChecks()
+//     .AddNpgSql(connectionString);
 
 var app = builder.Build();
+
+app.MapCarter();
+
+app.UseExceptionHandler(options => { });
+
+// app.MapHealthChecks("/health", new HealthCheckOptions
+// {
+//     ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+// });
 
 app.Run();
