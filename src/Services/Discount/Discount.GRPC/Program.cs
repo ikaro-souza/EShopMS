59da377a93ba4280e0cd85f91aad003bf8c98f41
ikaro-lisarb <ikaro.lisarb@proton.me>
2025-09-14 15:10:51 -0300

    feat: add DiscountContext and initial migration for Coupon model with CRUD operations

--- src/Services/Discount/Discount.GRPC/Program.cs
+++ src/Services/Discount/Discount.GRPC/Program.cs
@@ -1,12 +1,18 @@
+using Discount.GRPC.Data;
 using Discount.GRPC.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
 
+builder.Services.AddDbContext<DiscountContext>(opts =>
+    opts.UseSqlite(builder.Configuration.GetConnectionString("Database")));
+
 var app = builder.Build();
 
+app.UseMigration();
 app.MapGrpcService<DiscountService>();
 
 app.Run();
--- src/Services/Discount/Discount.GRPC/Services/DiscountService.cs
+++ src/Services/Discount/Discount.GRPC/Services/DiscountService.cs
@@ -1,18 +1,42 @@
+using Discount.GRPC.Data;
+using Discount.GRPC.Models;
 using Grpc.Core;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
 
 namespace Discount.GRPC.Services;
 
-public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+public class DiscountService(DiscountContext dbContext, ILogger<DiscountService> logger)
+    : DiscountProtoService.DiscountProtoServiceBase
 {
-    public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
     {
-        // TODO: Get it from database
-        return base.GetDiscount(request, context);
+        var coupon = await dbContext.Coupons.FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
+        if (coupon is null)
+        {
+            logger.LogInformation("Discount with ProductName={ProductName} not found", request.ProductName);
+            coupon = new Coupon
+            {
+                ProductName = "No Discount",
+                Amount = 0,
+                Description = "No Discount"
+            };
+        }
+
+        return coupon.Adapt<CouponModel>();
     }
 
-    public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
     {
-        return base.CreateDiscount(request, context);
+        var coupon = request.Coupon.Adapt<Coupon>();
+        if (coupon is null)
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid request object."));
+
+        dbContext.Coupons.Add(coupon);
+        await dbContext.SaveChangesAsync();
+
+        logger.LogInformation("Discount with ProductName={ProductName} successfully created", coupon.ProductName);
+        return coupon.Adapt<CouponModel>();
     }
 
     public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
