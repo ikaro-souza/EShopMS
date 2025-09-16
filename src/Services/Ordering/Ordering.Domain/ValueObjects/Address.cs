bd8f1d5b75e50de3404006160f4f77c32bafb5f0
ikaro-lisarb <ikaro.lisarb@proton.me>
2025-09-15 22:06:50 -0300

    feat: implement all the other value object static instanciation methods

--- src/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
@@ -2,11 +2,36 @@ namespace Ordering.Domain.ValueObjects;
 
 public record Address
 {
+    protected Address()
+    {
+    }
+
+    private Address(string firstName, string lastName, string? email, string addressLine, string country, string state,
+        string zipCode)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        AddressLine = addressLine;
+        Country = country;
+        State = state;
+        ZipCode = zipCode;
+    }
+
     public string FirstName { get; }
     public string LastName { get; }
-    public string? Email { get; }
+    public string Email { get; }
     public string AddressLine { get; }
     public string Country { get; }
     public string State { get; }
     public string ZipCode { get; }
+
+    public static Address Of(string firstName, string lastName, string email, string addressLine,
+        string country, string state, string zipCode)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(email);
+        ArgumentException.ThrowIfNullOrEmpty(addressLine);
+
+        return new Address(firstName, lastName, email, addressLine, country, state, zipCode);
+    }
 }
--- src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Payment.cs
@@ -2,9 +2,32 @@ namespace Ordering.Domain.ValueObjects;
 
 public record Payment
 {
+    protected Payment()
+    {
+    }
+
+    private Payment(string cardHolder, string cardNumber, string expirationDte, string cvv, int paymentMethod)
+    {
+        CardHolder = cardHolder;
+        CardNumber = cardNumber;
+        ExpirationDte = expirationDte;
+        Cvv = cvv;
+        PaymentMethod = paymentMethod;
+    }
+
     public string? CardHolder { get; }
     public string CardNumber { get; }
     public string ExpirationDte { get; }
-    public string CVV { get; }
+    public string Cvv { get; }
     public int PaymentMethod { get; }
+
+    public static Payment Of(string cardHolder, string cardNumber, string expirationDte, string cvv, int paymentMethod)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(cardHolder);
+        ArgumentException.ThrowIfNullOrWhiteSpace(cardNumber);
+        ArgumentException.ThrowIfNullOrWhiteSpace(cvv);
+        ArgumentOutOfRangeException.ThrowIfNotEqual(cvv.Length, 3);
+
+        return new Payment(cardHolder, cardNumber, expirationDte, cvv, paymentMethod);
+    }
 }
