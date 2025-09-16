635b211852ac81566e6064f4d62b6c1f666fd997
ikaro-lisarb <ikaro.lisarb@proton.me>
2025-09-15 23:22:28 -0300

    feat: implement rich domain models

--- src/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -17,4 +17,52 @@ public class Order : Aggregate<OrderId>
         get => OrderItems.Sum(item => item.Price * item.Quantity);
         private set { }
     }
+
+    public static Order Create(CustomerId customerId, OrderName orderName, Address shippingAddress,
+        Address billingAddress, Payment payment)
+    {
+        var order = new Order
+        {
+            Id = OrderId.Of(Guid.NewGuid()),
+            CustomerId = customerId,
+            OrderName = orderName,
+            ShippingAddress = shippingAddress,
+            BillingAddress = billingAddress,
+            Payment = payment,
+            Status = OrderStatus.Pending
+        };
+
+        // TODO: Add domain event
+        // order.AddDomainEvent(new OrderCreatedDomainEvent(order));
+
+        return order;
+    }
+
+    public void Update(OrderName orderName, Address shippingAddress,
+        Address billingAddress, Payment payment, OrderStatus status)
+    {
+        OrderName = orderName;
+        ShippingAddress = shippingAddress;
+        BillingAddress = billingAddress;
+        Payment = payment;
+        Status = status;
+
+        // TODO: Add domain event
+        // AddDomainEvent(new OrderUpdatedDomainEvent(this));
+    }
+
+    public void AddOrderItem(ProductId productId, int quantity, decimal price)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var orderItem = new OrderItem(Id, productId, quantity, price);
+        _orderItems.Add(orderItem);
+    }
+
+    public void RemoveOrderItem(ProductId productId)
+    {
+        var orderItem = _orderItems.FirstOrDefault(item => item.ProductId == productId);
+        if (orderItem is not null) _orderItems.Remove(orderItem);
+    }
 }
--- src/Services/Ordering/Ordering.Domain/Models/Product.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -4,4 +4,18 @@ public class Product : Entity<ProductId>
 {
     public string Name { get; private set; }
     public decimal Price { get; private set; }
+
+    public static Product Create(ProductId id, string name, decimal price)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+
+        return new Product
+        {
+            Id = id,
+            Name = name,
+            Price = price
+        };
+    }
 }
