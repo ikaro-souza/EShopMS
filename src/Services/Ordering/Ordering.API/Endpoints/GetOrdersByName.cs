42324fb7babd9825ae4f284a5384be0982b9a7ac
ikaro-lisarb <ikaro.lisarb@proton.me>
2025-09-18 19:13:14 -0300

    feat: implement get orders by name endpoint

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
@@ -1,9 +1,21 @@
+using Ordering.Application.Orders.Queries.GetOrdersByName;
+
 namespace Ordering.API.Endpoints;
 
-public record GetOrdersByNameRequest(string OrderId);
+// public record GetOrdersByNameRequest(string OrderId);
 
 public record GetOrdersByNameResponse(string OrderId);
 
-public class GetOrdersByName
+public class GetOrdersByName : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders/name/{orderName}", async (string orderName, ISender sender) =>
+        {
+            var result = await sender.Send(new GetOrdersByNameQuery(orderName));
+            var response = result.Adapt<GetOrdersByNameResponse>();
+            
+            return Results.Ok(response);
+        });
+    }
 }
