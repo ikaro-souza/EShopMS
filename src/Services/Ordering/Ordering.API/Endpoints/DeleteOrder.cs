4440c5bd669e5386d07b5892de64136bce4c95b3
ikaro-lisarb <ikaro.lisarb@proton.me>
2025-09-18 19:09:40 -0300

    feat: implement delete order endpoint

--- src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -1,9 +1,21 @@
+using Ordering.Application.Orders.Commands.DeleteOrder;
+
 namespace Ordering.API.Endpoints;
 
-public record DeleteOrderRequest(string OrderId);
+// public record DeleteOrderRequest(string OrderId);
 
-public record DeleteOrderResponse(string OrderId);
+public record DeleteOrderResponse(bool IsSuccess);
 
-public class DeleteOrder
+public class DeleteOrder : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/orders/{id:guid}", async (Guid id, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteOrderCommand(id));
+            var response =  result.Adapt<DeleteOrderResponse>();
+            
+            return Results.Ok(response);
+        });
+    }
 }
