d43dc1c5e189bfd6f7b8055ea385015d0e89ec3c
ikaro-lisarb <ikaro.lisarb@proton.me>
2025-09-18 14:27:45 -0300

    feat: get orders by customer query

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerHandler.cs
@@ -0,0 +1,18 @@
+namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+
+public class GetOrdersByCustomerQueryHandler(IApplicationDbContext dbContext)
+    : IQueryHandler<GetOrdersByCustomerQuery, GetOrdersByCustomerResult>
+{
+    public async Task<GetOrdersByCustomerResult> Handle(GetOrdersByCustomerQuery request,
+        CancellationToken cancellationToken)
+    {
+        var orders = await dbContext.Orders.Include(o => o.OrderItems)
+            .AsNoTracking()
+            .Where(o => o.CustomerId.Equals(request.CustomerId))
+            .OrderBy(o => o.OrderName.Value)
+            .ToListAsync(cancellationToken);
+
+        var orderDtos = orders.Select(OrderDto.From).ToList();
+        return new GetOrdersByCustomerResult(orderDtos);
+    }
+}
