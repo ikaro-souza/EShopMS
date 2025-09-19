bfc16623c8add7eebc438a97d8f459db72b3692a
ikaro-lisarb <ikaro.lisarb@proton.me>
2025-09-19 19:20:08 -0300

    feat: add message broker building blocks

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/Events/BasketCheckoutEvent.cs
@@ -0,0 +1,24 @@
+namespace BuildingBlocks.Messaging.Events;
+
+public record BasketCheckoutEvent : IntegrationEvent
+{
+    public string UserName { get; set; } = string.Empty;
+    public Guid CustomerId { get; set; } = Guid.Empty;
+    public decimal TotalPrice { get; set; } = 0;
+
+    // Shipping and BillingAddress
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string AddressLine { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+    public string State { get; set; } = string.Empty;
+    public string ZipCode { get; set; } = string.Empty;
+
+    // Payment
+    public string CardHolder { get; set; } = string.Empty;
+    public string CardNumber { get; set; } = string.Empty;
+    public string ExpirationDate { get; set; } = string.Empty;
+    public string Cvv { get; set; } = string.Empty;
+    public int PaymentMethod { get; set; } = 0;
+}
