686efb0d8ab6e5433da0e2a7ff80837d883902cc
ikaro-lisarb <ikaro.lisarb@proton.me>
2025-09-30 22:16:00 -0300

    feat: add rate limiting to ordering service

--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,9 +1,20 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("fixed", fwlOptions =>
+    {
+        fwlOptions.Window = TimeSpan.FromSeconds(10);
+        fwlOptions.PermitLimit = 5;
+    });
+});
 
 var app = builder.Build();
 
+app.UseRateLimiter();
 app.MapReverseProxy();
 
 app.Run();
